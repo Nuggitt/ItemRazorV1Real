d257a4f428290c115673d6958bf9ab0a47cf350c
Nuggitt <111018991+Nuggitt@users.noreply.github.com>
2023-03-25 16:57:40 +0100

    JsonIconImageBootstrapItemsOPG5 DOne

---
+++ Service/JsonFileItemService.cs
@@ -0,0 +1,45 @@
+﻿using ItemRazorV1Real.Models;
+using System.Text.Json;
+
+namespace ItemRazorV1Real.Service
+{
+    public class JsonFileItemService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "Data", "Items.json"); }
+        }
+
+        public JsonFileItemService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public void SaveJsonItems(List<Item> items)
+        {
+            using (FileStream jsonFileWriter = File.Create(JsonFileName))
+            {
+                Utf8JsonWriter jsonWriter = new Utf8JsonWriter(jsonFileWriter, new JsonWriterOptions()
+                {
+                    SkipValidation = false,
+                    Indented = true
+                });
+                JsonSerializer.Serialize<Item[]>(jsonWriter, items.ToArray());
+            }
+        }
+
+        public IEnumerable<Item> GetJsonItems()
+        {
+            using (StreamReader jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Item[]>(jsonFileReader.ReadToEnd());
+            }
+        }
+
+
+
+
+    }
+}
