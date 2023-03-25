392201de0cad2f081c4396e3d95653b6cf739c0f
Nuggitt <111018991+Nuggitt@users.noreply.github.com>
2023-03-25 15:23:21 +0100

    CreateNewItemsOPG2 Done

---
+++ MockData/MockItems.cs
@@ -0,0 +1,19 @@
+﻿using ItemRazorV1Real.Models;
+
+namespace ItemRazorV1Real.MockData
+{
+    public class MockItems
+    {
+        private static List<Item> items = new List<Item>()
+        {
+            new Item(1,"PC", 5999),
+            new Item(2, "Skærm", 1999),
+            new Item(3, "Tastatur", 999)
+        };
+
+        public static List<Item> GetMockItems()
+        {
+            return items;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using ItemRazorV1Real.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSingleton<IItemService, ItemService>();
 
 var app = builder.Build();
 
