1217b082804d2c8fa644b1800c882e85c8a0f5a1
Nuggitt <111018991+Nuggitt@users.noreply.github.com>
2023-11-22 11:55:01 +0100

    added user class

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace ItemRazorV1Real.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {
+            
+        }
+
+        public User(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+    }
+}
