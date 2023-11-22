a0bff757072c2581c0744d430515e5d685b6460f
Nuggitt <111018991+Nuggitt@users.noreply.github.com>
2023-11-22 13:11:02 +0100

    yte

--- Pages/LogIn/LogOutPage.cshtml.cs
+++ Pages/LogIn/LogOutPage.cshtml.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,14 @@ namespace ItemRazorV1Real.Pages.LogIn
 {
     public class LogOutPageModel : PageModel
     {
-        public void OnGet()
+       
+        public async Task<IActionResult> OnGet()
         {
+            LogInPageModel.LoggedInUser = null;
+
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("/index");
         }
+
     }
 }
