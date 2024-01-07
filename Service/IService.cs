df46105cfcdb482922626797e70442472f371115
Nuggitt <111018991+Nuggitt@users.noreply.github.com>
2024-01-07 20:27:31 +0100

    Iservice interface

---
+++ Service/IService.cs
@@ -0,0 +1,12 @@
+﻿namespace ItemRazorV1Real.Service
+{
+    public interface IService<T>
+    {
+        Task<IEnumerable<T>> GetObjectsAsync();
+        Task AddObjectAsync(T obj);
+        Task DeleteObjectAsync(T obj);
+        Task UpdateObjectAsync(T obj);
+        Task <T> GetObjectByIdAsync(int id);
+        Task SaveObjects(List<T> objs);
+    }
+}
